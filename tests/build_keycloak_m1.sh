44a9843151e42056aedbc2cf68fca959f6382d6a
Yannic Schröer <yannicschroer@Yannics-MBP.fritz.box>
2021-12-21 12:14:48 +0100

    docs updated. Exceptions introduced

---
+++ tests/build_keycloak_m1.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+cd /tmp
+git clone git@github.com:keycloak/keycloak-containers.git
+cd keycloak-containers/server
+git checkout 16.1.0
+docker build -t "jboss/keycloak:16.0.1" .
+cd .. && cd ..
+rm -rf tmp
