936f72ad034e2093f2fce55d71df8f6e499556b5
Thomas Darimont <thomas.darimont@googlemail.com>
2022-02-10 11:33:46 +0100

    Make admin-client configurable
    Fixes #18
    
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- tests/app.py
+++ tests/app.py
@@ -11,6 +11,7 @@ idp = FastAPIKeycloak(
     server_url="http://localhost:8085/auth",
     client_id="test-client",
     client_secret="GzgACcJzhzQ4j8kWhmhazt7WSdxDVUyE",
+    admin_client_id="admin-cli",
     admin_client_secret="BIcczGsZ6I8W5zf0rZg5qSexlloQLPKB",
     realm="Test",
     callback_uri="http://localhost:8081/callback"
