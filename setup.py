b088ecee9bf580709aa64ae828575765ead1fabc
Michael Robinson <laxdog@gmail.com>
2022-08-10 12:48:00 +0100

    Add setup.py to allow editable installs, e.g. pip install -e .

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# This has only been added to allow editable dev installs, pyproject.toml replaces setup.py
+# e.g. pip install -e .
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
